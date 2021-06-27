d4d67937851db2995f6702d24b1c2ee73d32e468
gravitygrey-dev <hayden_grey@hotmail.co.nz>
2021-06-27 16:46:44 +1200

    Added a toggle button to used in darrk mode

---
+++ kords/src/Components/toggleMode.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ToggleControl = () => {
+    return (
+    <div>
+    <label className="switch">
+        <input type='checkbox' className="switch" />
+        <span className="slider round"></span>
+    </label>
+    </div>);
+}
+
+export default ToggleControl
